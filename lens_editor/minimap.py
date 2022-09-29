4fe0211405ddbd1e077e6d0945380e5b6f7c98ab
G-87 <615879004@qq.com>
2022-09-29 19:46:25 +0800

    能打开但张缺陷,lens还未传入

--- lens_editor/minimap.py
+++ lens_editor/minimap.py
@@ -7,12 +7,14 @@
 # from webbrowser import BackgroundBrowser
 # from xml.etree.ElementPath import xpath_tokenizer
 from os import get_blocking
+from socket import SO_BINDTODEVICE
 import cv2
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QPixmap, QImage, QBrush, QColor
 # from enum import Enum
 # from typing import Tuple
 import numpy as np
+# from lens_editor.rule_edit import RuleEditWindow
 # class Orientation(Enum):
 #     UP = 0
 #     DOWN = 1
@@ -62,7 +64,7 @@ def numpy2pixmap(np_img) -> QPixmap:
     ).rgbSwapped()
     return QPixmap(qimg)
 
-class Minimap:
+class Minimap():
     def __init__(self, defect,width,show_all=False):
         self.defect = defect
         self.width = width
@@ -73,24 +75,16 @@ class Minimap:
         self.draw_all(self)
 
     def draw(self,defect):
-        x_t =self.defect.xmax + 50
-        y_t = self.defect.ymin - 50
-        if x_t >=1200 and y_t<=1200:
-            self.first()
-        elif x_t<1200 and y_t<1200:
-            self.second()
-        elif x_t<1200 and y_t>1200:
-            self.third()
-        elif x_t>=1200 and y_t>=1200:
-            self.four()
+        x = self.defect.xmax
+        y = self.defect.ymin
         # dic = {
         #     "1":self.first(),
         #     "2":self.second(),
         #     "3":self.third(),
         #     "4":self.four()
         # }
-        cv2.circle(self.background,(defect.xmin, defect.ymin),20, (255,0,0))   #x,y用来画圈，x_t,y_t,用来画缩略图
-        # cv2.circle(self.background, (x, y), 25, 255,0,0, 3)       
+        cv2.circle(self.background,(defect.xmin, defect.ymin),20, (255,0,255))   #x,y用来画圈，x_t,y_t,用来画缩略图
+        cv2.circle(self.background, (x, y), 25, 255,0,0, 3)       
     
     def first(self):
         thick = 3
@@ -109,91 +103,41 @@ class Minimap:
             cv2.BORDER_CONSTANT | cv2.BORDER_ISOLATED,
             value=color,
         )
-        x_t = self.defect.xmax  -150
-        y_t = self.defect.ymin +40
-        self.background[
-            y_t : y_t + tooltip.shape[0],
-            x_t : x_t + tooltip.shape[1],
-        ] = tooltip
-        return numpy2pixmap(self.background).scaledToWidth(self.width, Qt.SmoothTransformation)
-    def second(self):
-        thick = 3
-        color = (0, 190, 246)
-        d_img = self.defect.image
-        d_w, d_h = d_img.shape[:2]
-        r_w = 100
-        r_h = int(r_w * d_w / d_h)
-        detail_img = cv2.resize(d_img, (r_w, r_h))
-        tooltip = cv2.copyMakeBorder(
-            detail_img,
-            thick,
-            thick,
-            thick,
-            thick,
-            cv2.BORDER_CONSTANT | cv2.BORDER_ISOLATED,
-            value=color,
-        )
-        x_t = self.defect.xmax  -50
-        y_t = self.defect.ymin +10
-        self.background[
-            y_t : y_t + tooltip.shape[0],
-            x_t : x_t + tooltip.shape[1],
-        ] = tooltip
-        return self.get_pixmap()
-    
-    def third(self):
-        thick = 3
-        color = (0, 190, 246)
-        d_img = self.defect.image
-        d_w, d_h = d_img.shape[:2]
-        r_w = 100
-        r_h = int(r_w * d_w / d_h)
-        detail_img = cv2.resize(d_img, (r_w, r_h))
-        tooltip = cv2.copyMakeBorder(
-            detail_img,
-            thick,
-            thick,
-            thick,
-            thick,
-            cv2.BORDER_CONSTANT | cv2.BORDER_ISOLATED,
-            value=color,
-        )
-        x_t = self.defect.xmax  +10
-        y_t = self.defect.ymin  -10-r_h
-        self.background[
-            y_t : y_t + tooltip.shape[0],
-            x_t : x_t + tooltip.shape[1],
-        ] = tooltip
-        return self.get_pixmap()
-    
-    def four(self):
-        thick = 3
-        color = (0, 190, 246)
-        d_img = self.defect.image
-        d_w, d_h = d_img.shape[:2]
-        r_w = 100
-        r_h = int(r_w * d_w / d_h)
-        detail_img = cv2.resize(d_img, (r_w, r_h))
-        tooltip = cv2.copyMakeBorder(
-            detail_img,
-            thick,
-            thick,
-            thick,
-            thick,
-            cv2.BORDER_CONSTANT | cv2.BORDER_ISOLATED,
-            value=color,
-        )
-        x_t = self.defect.xmax -(2*r_w)
-        y_t = self.defect.ymin -r_h-30
-        self.background[
-            y_t : y_t + tooltip.shape[0],
-            x_t : x_t + tooltip.shape[1],
-        ] = tooltip
-        return self.get_pixmap()
+        x_t =self.defect.xmax + 50
+        y_t = self.defect.ymin - 50
+        if x_t >=1200 and y_t<=1200:
+            x_t = self.defect.xmax  -150
+            y_t = self.defect.ymin +40
+            self.background[
+                y_t : y_t + tooltip.shape[0],
+                x_t : x_t + tooltip.shape[1],
+            ] = tooltip
+        elif x_t<1200 and y_t<1200:
+             x_t = self.defect.xmax  -50
+             y_t = self.defect.ymin +10
+             self.background[
+                y_t : y_t + tooltip.shape[0],
+                x_t : x_t + tooltip.shape[1],
+            ] = tooltip
+        elif x_t<1200 and y_t>1200:
+            x_t = self.defect.xmax  +10
+            y_t = self.defect.ymin  -10-r_h
+            self.background[
+                y_t : y_t + tooltip.shape[0],
+                x_t : x_t + tooltip.shape[1],
+            ] = tooltip
+        elif x_t>=1200 and y_t>=1200:
+             x_t = self.defect.xmax -(2*r_w)
+             y_t = self.defect.ymin -r_h-30
+             self.background[
+                y_t : y_t + tooltip.shape[0],
+                x_t : x_t + tooltip.shape[1],
+            ] = tooltip
+        return numpy2pixmap(self.background).scaledToWidth(self.width, Qt.SmoothTransformation) 
+
         
-    def draw_all(self,lens):
-        self.lens = lens
-        for d in self.lens:
+    def draw_all(self):
+        for d in self.lens.defects:
             self.draw(d)
 
     def get_pixmap(self) -> QPixmap:
